d9e02f3616c75d424a2a984ff3509a9cc4086a12
IOrozakunov23 <islamburunduk045>
2023-01-24 15:03:10 +0500

    home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,21 @@
 <template>
-  <div class="justify-center flex  mt-28">
-    <div class="bg-amber-500  w-10/12 h-4/5">
-      <div class="rounded-full justify-center flex ">
-        <div class="mt-24">
-          <button class="bg-gray-800 w-40 text-white h-12 rounded-md ml-12">Каталог</button>
-          <button class="bg-gray-800 w-40 text-white h-12 rounded-md  mt-28 ml-44">Новости</button>
+  <div class="justify-center flex ">
+    <div class="bg-amber-500 mt-20  w-10/12 h-4/5">
+      <div class="rounded-full justify-center flex">
+        <div class="grid grid-col-2 gap-[10px]">
+          <div class=" grid grid-cols-2  ">
+          <button class="bg-gray-800 w-40 text-white h-12 rounded-md">Каталог</button>
+          <button class="bg-gray-800 w-40 text-white h-12 rounded-md">о нас</button>
         </div>
-        <div class="block mr-24">
-          <img class="w-96 h-96" src="/src/assets/Ellipse 9.svg" />
-        </div>
-        <div class="mt-24 ">
-          <button class="bg-gray-800 text-white w-40 h-12 rounded-md ml-32">О нас</button>
-          <button class="bg-gray-800 text-white w-40 h-12 rounded-md mt-28 ">Контакты</button>
-        </div>
-
-      </div>1
+        <img class=" w-96 h-96" src="/src/assets/Ellipse 9.svg" />
+        <div class="grid grid-cols-2    ">
+          <button class="bg-gray-800 w-40 text-white h-12 rounded-md">ааxsа</button>
+          <button class="bg-gray-800 text-white w-40 h-12 rounded-md ">Контакты</button>
+        </div></div>
+    
+        
+       
+      </div>
     </div>
   </div>
 
