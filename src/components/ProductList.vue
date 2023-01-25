bbd5abedc895cc76bef5a769c44c94ed65681a98
unknown <abudzanboltaev@gmail.com>
2023-01-25 12:19:04 +0600

    product

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -6,7 +6,7 @@
             <h1 class="text-3xl">{{ item.name }}</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa repellen
               provident id! Excepturi.</p>
-              <button class="ml-[175px] w-[120px] h-[25px] rounded-[12px] text-black bg-slate-300 text-white">Купить</button>
+              <button class=" ml-[175px] w-[120px] h-[25px] rounded-[12px] text-black bg-slate-300 ">Купить</button>
         </div>
 
     </div>
