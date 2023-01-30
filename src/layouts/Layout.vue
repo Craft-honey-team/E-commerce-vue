9aa72aaa839d2373f1165b20f72a2c67f6edcc3d
akuFU <nikita.akulenko95@gmail.com>
2023-01-31 03:25:05 +0600

    wtf

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -1,10 +1,9 @@
 <template>
 
-<div class = "px-[5%] min-h-screen grid grid-rows-[min-content_auto_min-content] max-[800px]:px-[2%] absolute inset-0 overflow-hidden">
+
 <Header />
 <slot />
 <Footer />
-</div>
 
 </template>
 
