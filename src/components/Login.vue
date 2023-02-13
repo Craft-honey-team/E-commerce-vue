8636fdd8f2ca49def323452d85d942b4e925f378
AntonLobanoff <anton28lobanov@gmail.com>
2023-02-13 16:30:12 +0300

    newregisterform

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,28 +1,46 @@
 <template>
-        <div class="grid grid-cols-2" >
+    <div class="grid grid-cols-2">
 
-            <div class=" flex flex-col ">
-                <div class="mb-[20px] text-[30px] h-[45px] flex justify-center ">
-                  <span class = "bg-gray-300 rounded-[10px] px-2 text-black">Вход</span>
-                  <span class = "mx-2">|</span>
-                  <button @click = "store.comp = 'Register'" class = "bg-blue-500 rounded-[10px] px-2 text-white">Регистрация</button>  
-                </div>
-                
-                <button class="mb-[20px] border-2 flex items-center justify-center  rounded-[3px] border-none bg-[#E3EDFE] text-black h-[40px]"> <img src="/src/assets/Vector.svg"> Вход через Google
-                </button>
-                <input v-model="userName" class="border-2 mb-[20px] rounded-[3px] h-[45px] border-gray-400 " type="email"
-                    placeholder="  Ведите логин" />
-                <input v-model="password" class="border-2 rounded-[3px] h-[45px] border-gray-400 " type="password"
-                    placeholder=" Ведите пароль" />
-                    <router-link to="/" class=" text-blue-500 mb-[20px] flex justify-end">Забыли пароль?</router-link>
-                    <button @click = "userCheck" :disabled=" userName<=5 || password<=1" class="bg-blue-500 mb-[8%] border-2 border-none rounded-[3px] text-white h-[40px] disabled:opacity-60 ">Войти
-                    </button>
-                    <p>У вас уже есть аккаунт?<router-link to="/" class="text-blue-500">Войти</router-link></p>
-                    
+        <div class=" flex flex-col ">
+            <div class="mb-[40px] text-[30px] h-[45px] flex justify-center ">
+                <span class="bg-gray-300 rounded-[8px] px-2 text-black font-roboto  hover:text-amber-700">Вход</span>
+                <span class="mx-2">|</span>
+                <button @click="store.comp = 'Register'"
+                    class="bg-blue-500 rounded-[10px] px-2 text-white font-roboto  hover:text-amber-700">Регистрация</button>
             </div>
 
+            <button
+                class="mb-[20px] font-roboto border-2 flex items-center justify-center  rounded-[3px] border-none bg-[#E3EDFE] text-black h-[40px]  hover:text-amber-700"><img
+                    class="mr-[10px]" src="/src/assets/Vector.svg">Вход через Google
+            </button>
+            <input v-model="userName"
+                class="w-[300px] px-[8px] border-2 mb-[10px] rounded-[3px] h-[45px] border-gray-400 font-roboto "
+                type="email" placeholder="Введите логин" />
+            <div class="relative mb-[10px]">
+                <input v-model="password"
+                    class=" w-[300px] px-[8px] flex items-center font-roboto border-2 rounded-[3px] h-[45px] border-gray-400"
+                    required v-bind:type="showPassword ? 'text' : 'password'" placeholder="Введите пароль" />
+                <button @click="showPassword = !showPassword"
+                    class="buttonPass absolute right-[30px] top-0 bottom-0"><img src="src/assets/visibility_off.svg"
+                        alt=""></button>
+
+            </div>
+            <router-link to="/"
+                class=" text-blue-500 mb-[20px] flex justify-end  hover:text-amber-700 hover:underline underline-offset-4 ">Забыли
+                пароль?</router-link>
+            <button @click="userCheck" :disabled="userName <= 5 || password <= 1"
+                class="bg-blue-500 mb-[8%] border-2 border-none rounded-[3px] text-white h-[40px] disabled:opacity-60 ">Войти
+            </button>
+            <p>У вас уже есть аккаунт?<router-link to="/"
+                    class="ml-[8px] text-blue-500 underline hover:text-amber-700">Войти</router-link></p>
+
         </div>
+
+    </div>
 </template>
+<style>
+
+</style>
 
 <script>
 import { useStore } from '@/stores/test'
@@ -36,12 +54,13 @@ export default {
             But: false,
             userName: "",
             password: null,
-            store: useStore()
+            store: useStore(),
+            showPassword: false
         }
     },
-    
-        
-   
+
+
+
 }
 
 </script>
--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,31 +1,51 @@
 <template>
 
-    <div class="grid grid-cols-2">
-    
-      <div class="flex flex-col">
-        <div class="mb-[8px] text-[30px] h-[45px] flex justify-center ">
-          <button @click = "store.comp = 'Login'" class = "bg-blue-500 rounded-[10px] px-2 text-white">Вход</button>  
-          <span class = "mx-2">|</span>
-          <span class = "bg-gray-300 rounded-[10px] px-2 text-black">Регистрация</span>
-        </div>
-        <button
-          class="mb-[20px] border-2 flex items-center justify-center  rounded-[3px] border-none bg-[#E3EDFE] text-black h-[40px]">
-          <img class="mr-[8px]" src="@/assets/Googlelogo.svg"> Вход через Google
-        </button>
-        <input v-model="name" class=" border-2 mb-[10px] rounded-[3px] h-[45px] border-gray-400 " type="text"
-          placeholder="Имя пользователя">
-        <input v-model="email" class="border-2 rounded-[3px] mb-[10px] h-[45px] border-gray-400 " type="email"
-          placeholder="Email или телефон" />
-        <input v-model="password" class="bg border-2 rounded-[3px] mb-[10px] h-[45px] border-gray-400 "
-          type="password" placeholder="Пароль" />
-        <input v-model="password2" class=" mb-[30px]  border-2 rounded-[3px] h-[45px] border-gray-400 "
-          type="password"  placeholder="Подвердите пароль" /> 
-        <button @click="register" :disabled=" name<=2 || password<=2 || email<=5 || password!==password2"
-         class=" bg-blue-500 border-2 border-none rounded-[3px] text-white h-[40px] disabled:opacity-60 " >Войти
-        </button>
+  <div class="grid grid-cols-2">
+
+    <div class="flex flex-col">
+      <div class="mb-[40px] text-[30px] h-[45px] flex justify-center ">
+        <button @click="store.comp = 'Login'"
+          class="bg-blue-500 rounded-[10px] px-2 text-white  hover:text-amber-700">Вход</button>
+        <span class="mx-2">|</span>
+        <span class="bg-gray-300 rounded-[10px] px-2 text-black  hover:text-amber-700">Регистрация</span>
+      </div>
+      <button
+        class="mb-[20px] font-roboto border-2 flex items-center justify-center  rounded-[3px] border-none bg-[#E3EDFE] text-black h-[40px]  hover:text-amber-700">
+        <img class="mr-[8px]" src="@/assets/Googlelogo.svg"> Вход через Google
+      </button>
+      <input v-model="name" class=" px-[8px] font-roboto border-2 mb-[10px] rounded-[3px] h-[45px] border-gray-400 "
+        type="text" placeholder="Имя пользователя">
+      <input v-model="email" class="px-[8px] font-roboto border-2 rounded-[3px] mb-[10px] h-[45px] border-gray-400 "
+        type="email" placeholder="Email или телефон" />
+      <!-- <input v-model="password" class="px-[8px] font-roboto bg border-2 rounded-[3px] mb-[10px] h-[45px] border-gray-400 "
+          type="password" placeholder="Пароль" /> -->
+      <div class="relative mb-[10px]">
+        <input v-model="password"
+          class=" w-[300px] px-[8px] flex items-center font-roboto border-2 rounded-[3px] h-[45px] border-gray-400"
+          required v-bind:type="showPassword ? 'text' : 'password'" placeholder="Пароль" />
+        <button @click="showPassword = !showPassword" class="buttonPass absolute right-[30px] top-0 bottom-0"><img
+            src="src/assets/visibility_off.svg" alt=""></button>
 
       </div>
+      <!-- <input v-model="password2"
+        class=" px-[8px] font-roboto mb-[30px]  border-2 rounded-[3px] h-[45px] border-gray-400 " type="password"
+        placeholder="Подвердите пароль" /> -->
+
+      <div class="relative mb-[10px]">
+        <input v-model="password2"
+          class=" w-[300px] px-[8px] flex items-center font-roboto border-2 rounded-[3px] h-[45px] border-gray-400"
+          required v-bind:type="showPassword ? 'text' : 'password'" placeholder="Подвердите пароль" />
+        <button @click="showPassword = !showPassword" class="buttonPass absolute right-[30px] top-0 bottom-0"><img
+            src="src/assets/visibility_off.svg" alt=""></button>
+
+      </div>
+
+      <button @click="register" :disabled="name <= 2 || password <= 2 || email <= 5 || password !== password2"
+        class=" bg-blue-500 border-2 border-none rounded-[3px] text-white h-[40px] disabled:opacity-60 ">Войти
+      </button>
+
     </div>
+  </div>
 
 
 
@@ -43,18 +63,18 @@ export default {
       email: '',
       showPassword: false,
       store: useStore()
-      
+
     }
   },
   mounted() {
     console.log(import.meta.env.VITE_FIREBASE_API_KEY)
   },
   methods: {
-    async register () {
+    async register() {
       let obj = {
         method: 'POST',
         header: {
-          'Contetnt-Type':'application/json'
+          'Contetnt-Type': 'application/json'
         },
         body: JSON.stringify({
           email: this.email,
@@ -62,18 +82,17 @@ export default {
           returnSecureToken: true,
         })
       }
-      if (this.password2 == this.password){
-        
+      if (this.password2 == this.password) {
+
         const response = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${import.meta.env.VITE_FIREBASE_API_KEY}`, obj);
         const res = response.json();
         console.log(res);
-        
+
       }
     }
+  }
+}
 
-  }
-  
-  }
 
 //nikita.akulenko95@gmail.com
 </script>
