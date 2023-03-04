1ec5c167d258831831497f6a5173c62bb50d6e5e
AntonLobanoff <anton28lobanov@gmail.com>
2023-03-04 11:54:20 +0300

    allOk

--- src/views/PersonalArea.vue
+++ src/views/PersonalArea.vue
@@ -1,11 +1,11 @@
 <template>
-    <Layout>
+	<Layout>
 		<div>
 			<div class="pt-[20px] mt-[20px] mb-[40px] border-t-[3px] border-black">
 				<p class="text-[50px]">{{ store.langProp.personalarea }}</p>
 			</div>
 			<div class="grid grid-flow-row gap-20">
-				<div class="grid grid-flow-col auto-cols-max text-[20px] max-[700px]:grid-cols-1">
+				<div class="grid grid-flow-col auto-cols-max text-[20px] max-[700px]:grid-col-1">
 					<span
 						class="bg-gray-200 h-[30px] flex items-center font-bold font-roboto rounded-[10px] px-2 text-black text-center">{{
 							store.langProp.settings }}</span>
@@ -14,103 +14,148 @@
 						class="bg-[#EAAD02] h-[30px] rounded-[10px] px-2 text-white">{{ store.langProp.myorders
 						}}</button>
 				</div>
-				<div class="grid grid-flow-col auto-cols-max gap-[30px] text-[20px]">
-					<div class="text-slate-500 ">
+				<div class="grid grid-flow-col gap-[30px] text-[20px]">
+					<div class="text-slate-500">
 						<p class=" ">{{ store.langProp.yourname }}:</p>
 						<p class=" ">Email:</p>
 						<p class=" ">{{ store.langProp.telephone }}</p>
 						<p class=" ">{{ store.langProp.delivery }}:</p>
+						<p class="">Способ доставки: {{ }}</p>
 					</div>
 					<div>
 						<p class=" ">{{ store.name }}</p>
 						<p class=" ">{{ store.email }}</p>
 						<div v-show="setPhone">
-							<input @input="handleInput" v-model="store.number " placeholder="Введите номер телефона" class="border-solid border-2 border-grey-600 rounded-[10px] px-[5px] w-[190px] text-[14px] mr-[20px]">
-							<button @click="setPhone = false, savePhone(phone)" class="text-blue-700 underline decoration-1 text-[18px] hover:text-amber-700 hover:underline underline-offset-4">Добавить</button>
+							<input @input="handleInput" v-model="store.number" placeholder="Введите номер телефона"
+								class="border-solid border-2 border-grey-600 rounded-[10px] px-[5px] w-[190px] text-[14px] mr-[20px]">
+							<button @click="setPhone = false, savePhone(phone)"
+								class="text-blue-700 underline decoration-1 text-[18px] hover:text-amber-700 hover:underline underline-offset-4">Добавить</button>
 						</div>
 						<div v-show="!setPhone">
 							<span v-if="store.number.length" class="mr-[20px]">{{ store.number }}</span>
-							<button @click="setPhone = true" class="text-blue-700 underline decoration-1 text-[18px] hover:text-amber-700 hover:underline underline-offset-4">Изменить номер</button>
+							<button @click="setPhone = true"
+								class="text-blue-700 underline decoration-1 text-[18px] hover:text-amber-700 hover:underline underline-offset-4">Изменить
+								номер</button>
 						</div>
 						<div v-show="setAddress">
-							<input @input="handleInputAddress" v-model="store.address " placeholder="Введите адрес" class="border-solid border-2 border-grey-600 rounded-[10px] px-[5px] w-[190px] text-[14px] mr-[20px]">
-							<button @click="setAddress = false" class="text-blue-700 underline decoration-1 text-[18px] hover:text-amber-700 hover:underline underline-offset-4">Добавить</button>
+							<input @input="handleInputAddress" v-model="store.address" placeholder="Введите адрес"
+								class="border-solid border-2 border-grey-600 rounded-[10px] px-[5px] w-[190px] text-[14px] mr-[20px]">
+							<button @click="setAddress = false"
+								class="text-blue-700 underline decoration-1 text-[18px] hover:text-amber-700 hover:underline underline-offset-4">Добавить</button>
 						</div>
 						<div v-show="!setAddress">
 							<span v-if="store.address.length" class="mr-[20px]">{{ store.address }}</span>
-							<button @click="setAddress = true" class="text-blue-700 underline decoration-1 text-[18px] hover:text-amber-700 hover:underline underline-offset-4">Изменить адрес</button>
+							<button @click="setAddress = true"
+								class="text-blue-700 underline decoration-1 text-[18px] hover:text-amber-700 hover:underline underline-offset-4">Изменить
+								адрес</button>
 						</div>
-						
-						
-						
+						<div class="mt-[-15px]">
+							<div v-show="dhl">
+								<img class="w-[100px] h-[80px] " src="@/assets/dhl-express.svg" alt="">
+							</div>
+							<div v-show="dpd">
+								<img class="w-[100px] h-[80px] " src="@/assets/dpd-logo-2015-.svg" alt="">
+							</div>
+							<div v-show="cdek">
+								<img class="w-[100px] h-[80px] " src="@/assets/cdek-1.svg" alt="">
+							</div>
+						</div>
+
+					</div>
+					<div class="ml-[50px] gap-[10px] leading-4">
+						<div>
+							<h2>Выберите способ доставки:</h2>
+							<span class=" text-[14px] text-red-600 break-all w-[200px]">Внимание: бесплатная доставка
+								осуществляется при заказе на
+								сумму свыше 112500сом!</span><br />
+							<span class="text-[14px] text-red-600 break-all w-[90%]">При заказе на сумму ниже 112500сом
+								доставка осуществляется
+								за счет ПОКУПАТЕЛЯ по тарифам транспортной компании!</span>
+						</div>
+						<div class="grid grid-flow-col gap-[10px]">
+							<img @click="dhl = !dhl" class="w-[100px] h-[80px] hover:cursor-pointer"
+								src="@/assets/dhl-express.svg" alt="">
+							<img @click="dpd = !dpd" class="w-[100px] h-[80px] hover:cursor-pointer"
+								src="@/assets/dpd-logo-2015-.svg" alt="">
+							<img @click="cdek = !cdek" class="w-[100px] h-[80px] hover:cursor-pointer"
+								src="@/assets/cdek-1.svg" alt="">
+						</div>
+
 					</div>
 				</div>
-				<button @click="resetPassword" class="text-blue-700 underline decoration-1 text-[18px] hover:text-amber-700 hover:underline underline-offset-4" to="">Изменить пароль</button>
-				<button @click="exitPersonalArea" class="bg-[#EAAD02] h-[30px] w-[200px] rounded-[10px] px-2 text-center text-white  hover:text-amber-700 hover:underline underline-offset-4">Выйти из АКК</button>
-			</div>        
+				<button @click="resetPassword"
+					class="text-blue-700 underline decoration-1 text-[18px] hover:text-amber-700 hover:underline underline-offset-4"
+					to="">Изменить пароль</button>
+				<button @click="exitPersonalArea"
+					class="bg-[#EAAD02] h-[30px] w-[200px] rounded-[10px] px-2 text-center text-white  hover:text-amber-700 hover:underline underline-offset-4">Выйти
+					из АКК</button>
+			</div>
 		</div>
-    </Layout>
+	</Layout>
 </template>
 <script>
 import { useStore } from '@/stores/test';
 import { getAuth, signOut, sendPasswordResetEmail } from "firebase/auth";
 export default {
-    data() {
-        return {
-            store: useStore(),			
-			setPhone: false,			
+	data() {
+		return {
+			store: useStore(),
+			setPhone: false,
 			setAddress: false,
-			
-        }
-    },
-	methods: {
-		exitPersonalArea(){
-		const auth = getAuth();
-signOut(auth).then(() => {
-  // Sign-out successful.
-  this.store.uid = '';
-  this.store.loggedIn = false;
-  this.store.getCart();
-  this.$router.push('/ru/home');
-}).catch((error) => {
-  // An error happened.
-});
-	},
-	handleInput(){
-	this.store.number = this.store.number.replace(/[^0-9+]/g, '');
-	this.store.number = this.store.number.substr(0 , 16);
-	
-	},
-	handleInputAddress(){
+			dpd: false,
+			dhl: false,
+			cdek: false,
 
+		}
 	},
-	savePhone(phone){
-		fetch('/api/updateUsers', {
-				
+	methods: {
+		exitPersonalArea() {
+			const auth = getAuth();
+			signOut(auth).then(() => {
+				// Sign-out successful.
+				this.store.uid = '';
+				this.store.loggedIn = false;
+				this.store.getCart();
+				this.$router.push('/ru/home');
+			}).catch((error) => {
+				// An error happened.
+			});
+		},
+		handleInput() {
+			this.store.number = this.store.number.replace(/[^0-9+]/g, '');
+			this.store.number = this.store.number.substr(0, 16);
+
+		},
+		handleInputAddress() {
+
+		},
+		savePhone(phone) {
+			fetch('/api/updateUsers', {
+
 				method: 'POST',
 				headers: {
-				
+
 					'Content-Type': 'application/json'
-				
+
 				},
-				body: JSON.stringify({ 1: 'phone', 2: this.store.number, 3: this.store.uid})
-			
+				body: JSON.stringify({ 1: 'phone', 2: this.store.number, 3: this.store.uid })
+
 			})
-	},
-	resetPassword(){
-		const auth = getAuth();
-sendPasswordResetEmail(auth, this.email)
-  .then(() => {
-    // Password reset email sent!
-    // ..
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
-  });
+		},
+		resetPassword() {
+			const auth = getAuth();
+			sendPasswordResetEmail(auth, this.email)
+				.then(() => {
+					// Password reset email sent!
+					// ..
+				})
+				.catch((error) => {
+					const errorCode = error.code;
+					const errorMessage = error.message;
+					// ..
+				});
+		}
 	}
-}
 
 }
 
